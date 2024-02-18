03d8ebe1b7c26980c3e67cfdccddc7a903e3a4db
MrFluffyOven <137286588+MrFluffyOven@users.noreply.github.com>
2024-02-18 21:20:52 +0000

    Cleanup

--- device.mk
+++ device.mk
@@ -4,18 +4,28 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+# Inherit from common AOSP config
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 LOCAL_PATH := device/samsung/gta9p
+
+PRODUCT_PLATFORM := holi
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    update_engine \
+    update_engine_sideload \
+    update_verifier \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-service \
+    android.hardware.boot@1.0-impl-wrapper.recovery \
+    android.hardware.boot@1.0-impl-wrapper \
+    android.hardware.boot@1.0-impl.recovery \
+    bootctrl.$(PRODUCT_PLATFORM) \
+    bootctrl.$(PRODUCT_PLATFORM).recovery \
 
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
-
-# Partitions
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
-# QCOM
-PRODUCT_PACKAGES += \
-    qcom_decrypt \
-    qcom_decrypt_fbe
